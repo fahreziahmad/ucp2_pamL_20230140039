c8149d05ef4f2777cb04bdb98d56992e72186a03
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-05-13 00:34:07 +0700

    feat: Menambahkan status AuthRegisterSuccess untuk alur registrasi

--- lib/blocs/auth/auth_state.dart
+++ lib/blocs/auth/auth_state.dart
@@ -1,31 +1,31 @@
 import 'package:equatable/equatable.dart';
-import '../../data/models/user_model.dart';
 
 abstract class AuthState extends Equatable {
   const AuthState();
-
+  
   @override
   List<Object?> get props => [];
 }
 
 class AuthInitial extends AuthState {}
-
 class AuthLoading extends AuthState {}
 
-class Authenticated extends AuthState {
-  final UserModel user;
-  const Authenticated(this.user);
-
+class AuthAuthenticated extends AuthState {
+  final String username;
+  const AuthAuthenticated(this.username);
+  
   @override
-  List<Object?> get props => [user];
+  List<Object?> get props => [username];
 }
 
-class Unauthenticated extends AuthState {}
+class AuthUnauthenticated extends AuthState {}
 
-class AuthFailure extends AuthState {
+class AuthRegisterSuccess extends AuthState {} // Status baru: Berhasil Daftar
+
+class AuthError extends AuthState {
   final String message;
-  const AuthFailure(this.message);
-
+  const AuthError(this.message);
+  
   @override
   List<Object?> get props => [message];
 }
